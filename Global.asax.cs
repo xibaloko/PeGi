ecea2e31542aeb27b739d97c6a4ebd3a5d80dfb6
Ulisses Inacio <ulissesmilitao@hotmail.com>
2021-05-11 12:22:44 -0300

    Adicionando arquivos ao projeto e instalando bibliotecas necessarias

---
+++ Global.asax.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Web;
+using System.Web.Routing;
+
+
+namespace PeGi
+{
+    public class Global : HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            // Código que é executado na inicialização do aplicativo
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Home.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="PeGi.Home" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>TESTE HOME COM MASTERPAGE</h1>
+</asp:Content>
